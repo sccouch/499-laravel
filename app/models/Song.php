b8d7c26e9e60b3b293809969ee5aa3820af61f12
Sam Couch <sccouch@gmail.com>
2014-02-11 21:06:24 -0800

    initial commit

---
+++ app/models/Song.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sccouch
+ * Date: 2/11/14
+ * Time: 5:58 PM
+ */
+
+class Song {
+
+    public static function search($song_title, $artist)
+    {
+     /*
+     * SELECT * FROM songs
+     * INNER JOIN artists
+     * ON songs.artist_id = artists.id
+     * INNER JOIN genres
+     * ON songs.genre_id = genres.id
+     */
+        $query = DB::table('songs')
+            ->select('title', 'artist_name', 'genre', DB::raw("DATE_FORMAT(added, '%b %d %Y %h:%i %p') AS added"))
+            ->join('artists', 'artists.id', '=', 'songs.artist_id')
+            ->join('genres', 'songs.genre_id', '=', 'genres.id');
+
+        if($song_title) {
+            $query->where('title', 'LIKE', "%$song_title%");
+        }
+
+        if($artist) {
+            $query->where('artist_name', 'LIKE', "%$artist%");
+        }
+
+        $songs = $query->get();
+
+
+
+        return $songs;
+    }
+}
---
+++ app/views/songs/songs-list.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>Song Search</title>
+</head>
+<body>
+
+<h1>My Songs</h1>
+
+<?php foreach($songs as $song) : ?>
+    <div class="song">
+        <h4>
+            <?php echo $song->title ?> by <?php echo $song->artist_name; ?></h4>
+        </h4>
+        <p>Genre: <?php echo $song->genre; ?></p>
+        <p>Added: <?php echo $song->added; ?></p>
+    </div>
+<?php endforeach; ?>
+
+</body>
+</html>
